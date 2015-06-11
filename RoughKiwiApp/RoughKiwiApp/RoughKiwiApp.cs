8540b52abaa143e43ce3784a56bbd1064f7d3a0e
Roy Stijsiger <roy.stijsiger@outlook.com>
2015-06-11 10:22:50 +0200

    Added model for fruit Added some private variables

--- RoughKiwiApp/RoughKiwiApp/RoughKiwiApp.cs
+++ RoughKiwiApp/RoughKiwiApp/RoughKiwiApp.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-
+using System.Net.Http;
 using Xamarin.Forms;
 
 namespace RoughKiwiApp
 {
+	
 	public class App : Application
 	{
 		public App ()
@@ -36,6 +37,17 @@ namespace RoughKiwiApp
 		{
 			// Handle when your app resumes
 		}
+
+		private HttpClient _client = new HttpClient();
+		private static string _url = "http://10.10.35.24/api/products";
+		private ListView _listView;
+
+	}
+
+	public class Fruit{
+		public int Id{get; set;}
+		public string Name{get; set;}
+		public string Color{get; set;}
 	}
 }
 
